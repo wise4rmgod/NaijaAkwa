90b5693ff4793c5b1c2c207750abc1641dabb9b9
Wise4rmGod <wise4rmgod@gmail.com>
2018-11-30 00:40:26 +0100

    Initial commit

---
+++ app/src/main/java/com/developer/wise4rmgod/naijaakwa/SplashScreenOne.java
@@ -0,0 +1,13 @@
+package com.developer.wise4rmgod.naijaakwa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen_one);
+    }
+}
